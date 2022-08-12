19eeb26ca18646a66ec9a4cd4cc7181ce688da09
klluu <kevinluu228@gmail.com>
2022-08-11 18:41:08 -0700

    StartMenu Volume finished

--- Duality/Assets/Scripts/AudioManager.cs
+++ Duality/Assets/Scripts/AudioManager.cs
@@ -10,12 +10,22 @@ public class AudioManager : MonoBehaviour
 
     void Start()
     {
-
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", .8f);
+            Load();
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("musicVolume", .8f);
+            Load();
+        }
     }
 
     public void ChangeVolume()
     {
         AudioListener.volume = volumeSlider.value;
+        Save();
     }
 
     private void Load()
